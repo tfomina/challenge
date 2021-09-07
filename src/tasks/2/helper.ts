ed91a55e71ff67bec44fa06f17ea23a457507ada
tfomina <tatyana.fomina.1986@gmail.com>
2021-09-07 14:43:13 +0300

    Second task - fix parseDate function

--- src/tasks/2/helper.ts
+++ src/tasks/2/helper.ts
@@ -1,4 +1,6 @@
 export const parseDate = (created: string) => {
+    // return new Date(created).toLocaleString();
+
     const date = new Date(created);
 
     const year = date.getFullYear();
@@ -6,7 +8,8 @@ export const parseDate = (created: string) => {
         date.getMonth() < 9
             ? `0${date.getMonth() + 1}`
             : `${date.getMonth() + 1}`;
-    const day = date.getDay() < 10 ? `0${date.getDay()}` : `${date.getDay()}`;
+    const day =
+        date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
 
     const hours =
         date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
@@ -19,5 +22,5 @@ export const parseDate = (created: string) => {
             ? `0${date.getSeconds()}`
             : `${date.getSeconds()}`;
 
-    return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+    return `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}`;
 };
